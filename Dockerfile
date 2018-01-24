cf7bf24f3cbda2ef8211202df02f93b8e3223eb7
Kalluru <sk185505@ncr.com>
2018-01-24 16:04:59 +0530

    Updating Code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM centos
 COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY AWS /AWS
 RUN chmod +x /docker-entrypoint.sh
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 
 source ./build-params 
+rm -rf AWS
+git clone --depth=1 https://github.com/linuxautomations/AWS.git 
+rm -rf AWS/.git
 docker build -t $REPO_NAME:$TAG .
 docker push $REPO_NAME:$TAG
